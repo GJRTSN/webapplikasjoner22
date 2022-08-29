01cd169879bb4fb853500ed74564b939bf2cc89f
GJRTSN <98411330+GJRTSN@users.noreply.github.com>
2022-08-29 11:33:56 +0200

    Update index.js

--- fullstaekk_oppgaver/intro_javascript/oppgave0/index.js
+++ fullstaekk_oppgaver/intro_javascript/oppgave0/index.js
@@ -1,8 +1,32 @@
 // Oppgave 1
+const text = document.getElementById("remove")
+const button = document.getElementById("remove-btn")
+
+button.addEventListener("click", function() {
+    text.hidden = true;
+})
+
 // Oppgave 2
+const originalText = document.getElementById("change")
+const changeBtn = document.getElementById("change-btn")
+
+changeBtn.addEventListener("click", function () {
+    originalText.textContent = "Denne teksten har nå blitt endret";
+})
+
 // Oppgave 3
+const textPrint = document.getElementById("input-text")
+const textInput = document.querySelector("input")
+
+textInput.addEventListener("keyup", function (e) {
+    textPrint.textContent = e.target.value;
+})
+
 // Oppgave 4
 const myList = ["item one", "item two", "item three"];
+
+
+
 // Oppgave 5
 // Oppgave 6
 // Oppgave 7
